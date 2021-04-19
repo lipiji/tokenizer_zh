24738069a581aa76fe480b4d333d45d05fcc79a5
Piji Li <pagelee.sd@gmail.com>
2021-04-19 09:07:38 +0800

    Update gptx_tokenization.py

--- gptx_tokenization.py
+++ gptx_tokenization.py
@@ -1,5 +1,4 @@
 # coding=utf-8
-# Copyright 2018 The Open AI Team Authors and The HuggingFace Inc. team.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -13,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-"""Tokenization classes for OpenAI GPT."""
+"""Tokenization classes for GPTX."""
 
 import sys
 import os
